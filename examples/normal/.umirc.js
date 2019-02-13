203433f4d0b28126947633e90fce06147826fff9
chencheng (云谦) <sorrycc@gmail.com>
2019-02-13 16:12:32 +0800

    feat: validate options with ajv (#9)

--- examples/normal/.umirc.js
+++ examples/normal/.umirc.js
@@ -1,7 +1,7 @@
 
 export default {
   plugins: [
-    ['../../index', {
+    ['../../dist/index', {
       splitChunks: true,
     }],
   ],
---
+++ src/schema.ts
@@ -0,0 +1,36 @@
+
+export default {
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    entry: {
+      type: 'object'
+    },
+    htmlName: {
+      type: 'string',
+      minLength: 1,
+    },
+    splitChunks: {
+      anyOf: [
+        { type: 'boolean' },
+        { type: 'object' },
+      ],
+    },
+    html: {
+      type: 'object',
+      properties: {
+        template: {
+          type: 'string',
+          minLength: 1,
+          pattern: '.ejs$',
+        },
+      },
+    },
+    selectEntry: {
+      anyOf: [
+        { type: 'boolean' },
+        { type: 'object' },
+      ],
+    },
+  },
+};
